965978ca1f636b9c8c311be65b966cd1ef562e15
itsthistim <timhofmann01@gmail.com>
2025-07-19 16:03:17 +0200

    feat: use expressive code integration and update reset and global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon()]
+	integrations: [
+		icon(),
+		expressiveCode({
+			themes: ["catppuccin-latte", "catppuccin-mocha"]
+		})
+	]
 });
